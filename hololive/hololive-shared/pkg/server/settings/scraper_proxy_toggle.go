c69e52208e5968fa908a22bc94dae02258f2168f
kapu <pjoonwoo111@gmail.com>
2026-04-09 22:50:03 +0900

    refactor(shared): dedupe scraper proxy toggle helper

---
+++ hololive/hololive-shared/pkg/server/settings/scraper_proxy_toggle.go
@@ -0,0 +1,38 @@
+package settings
+
+import (
+	"log/slog"
+
+	"github.com/kapu/hololive-shared/pkg/service/holodex"
+	"github.com/kapu/hololive-shared/pkg/service/youtube"
+	"github.com/kapu/hololive-shared/pkg/service/youtube/poller"
+)
+
+func ApplyScraperProxyToggle(
+	enabled bool,
+	youtubeService youtube.Service,
+	holodexService *holodex.Service,
+	scraperScheduler *poller.Scheduler,
+	logger *slog.Logger,
+) {
+	youtubeApplied := false
+	holodexApplied := false
+	schedulerApplied := 0
+
+	if youtubeService != nil {
+		youtubeApplied = youtubeService.SetScraperProxyEnabled(enabled)
+	}
+	if holodexService != nil {
+		holodexApplied = holodexService.SetScraperProxyEnabled(enabled)
+	}
+	if scraperScheduler != nil {
+		schedulerApplied = scraperScheduler.SetProxyEnabled(enabled)
+	}
+
+	logger.Info("Applied scraper proxy toggle",
+		slog.Bool("enabled", enabled),
+		slog.Bool("youtube_applied", youtubeApplied),
+		slog.Bool("holodex_applied", holodexApplied),
+		slog.Int("scheduler_pollers_applied", schedulerApplied),
+	)
+}
