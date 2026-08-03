4609796ce4b120dd0e96ab0b6898518c7704b5ea
park285 <pjoonwoo111@gmail.com>
2026-08-03 18:49:31 +0900

    refactor(delivery): dispatcher blocking lifecycle 추가

---
+++ hololive/hololive-shared/pkg/service/delivery/dispatcher_run.go
@@ -0,0 +1,19 @@
+package delivery
+
+import (
+	"context"
+
+	"github.com/kapu/hololive-shared/pkg/panicguard"
+)
+
+// Run은 dispatcher loop를 caller goroutine에서 실행해 종료와 panic 결과를 lifecycle owner에 반환합니다.
+func (d *Dispatcher) Run(ctx context.Context) error {
+	if d == nil {
+		return nil
+	}
+
+	return panicguard.RunE(d.logger, "delivery-dispatcher", func() error {
+		d.run(ctx)
+		return nil
+	})
+}
